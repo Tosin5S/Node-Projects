af77cbee6d1c82c077a00687ac680f894cb23e95
x5s <olyguy.aq@gmail.com>
2022-01-29 13:53:28 +0100

    connected to mongodb using mongoose library

---
+++ moongoose-mongodb/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./user.route');
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use('/user', userRouter);
+
+// Replace uri with connection string from mongodb
+const uri = "connection string";
+
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
